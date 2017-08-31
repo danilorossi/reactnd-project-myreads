848735c8e6aa1cf6af03d8a76720bd734f00c4ba
Danilo Rossi <contact@danilorossi.me>
2017-08-31 19:21:21 +0200

    refactor: Add a Book component

---
+++ src/components/common/Book.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { PropTypes } from 'prop-types'
+
+class Book extends React.Component {
+
+  render() {
+    return (
+      <div className="book">
+        <div className="book-top">
+
+          <div className="book-cover"
+            style={{
+              width: 128, height: 193,
+              backgroundImage: `url("${ this.props.imageURL }")`
+            }}></div>
+
+          <div className="book-shelf-changer">
+            <select>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{ this.props.title }</div>
+        <div className="book-authors">{ this.props.authors }</div>
+      </div>
+    )
+  }
+}
+
+Book.propTypes = {
+  imageURL: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  authors: PropTypes.string.isRequired
+};
+
+export default Book;
